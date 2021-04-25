b4f0140f64366064c1599ec6801acfff299cad89
Dgryzhkov <dgryzhkov@gmail.com>
2021-04-25 14:51:56 +0700

    refactoring code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
 const val SECOND = 60
 
 fun main() {
-    val timeAgo = SECOND * 11// веди число (минуты)
+    val timeAgo = SECOND*SECOND*21 // веди число (минуты)
 
-    println(printending(timeAgo))
+    println(printEnding(timeAgo))
 }
 
