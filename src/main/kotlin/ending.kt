d7da4019a7cbf8d52b7b1ac41bdbc5659568bf88
Dgryzhkov <dgryzhkov@gmail.com>
2021-04-25 07:58:30 +0700

    bug fixes

--- src/main/kotlin/ending.kt
+++ src/main/kotlin/ending.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 const val HOURS = "час"
 const val MINUTES = "минут"
 const val ENDING_HOURS_A = "а"
@@ -5,15 +7,36 @@ const val ENDING_HOURS_OV = "ов"
 const val ENDING_MINUTES_YOU = "ы"
 const val ENDING_MINUTE_U = "у"
 
-fun minutesOrHours(time: Int): String {
-    val end = when (time) {
-        in 0..3599 -> MINUTES
-        else -> HOURS
+
+fun endMinutes(time: Int): String {
+    val end = when (time / SECOND % 10) {
+        0, in 5..9 -> MINUTES
+        1 -> if (time / SECOND == 11) MINUTES else MINUTES + ENDING_MINUTE_U //else HOURS
+        else -> MINUTES + ENDING_MINUTES_YOU
+    }
+    return end
+}
+
+fun endHours(time: Int): String {
+    val end = when (time / (SECOND * SECOND) % 10) {
+        0, in 5..9 -> HOURS + ENDING_HOURS_OV
+        1 -> if (((time / (SECOND * SECOND)) % 100) == 11) HOURS + ENDING_HOURS_OV else HOURS
+        else -> HOURS + ENDING_HOURS_A
     }
     return end
 }
 
 fun ending(time: Int): String {
-    val end = if (minutesOrHours(time) == MINUTES) elevenMinutes(time) else elevenHours(time)
-    return end
+    return if (time in 0..3599) endMinutes(time) else endHours(time)
+}
+
+fun printending(time: Int): String {
+    return when {
+        time <= SECOND -> "был(а) только что"
+        time < SECOND * SECOND -> "был(а) ${time / SECOND} ${ending(time)} назад"
+        time < 24 * SECOND * SECOND -> "был(а) в сети ${time / (SECOND * SECOND)} ${ending(time)} назад"
+        time < 48 * SECOND * SECOND -> "был(а) сегодня"
+        time < 72 * SECOND * SECOND -> "был(а) вчера"
+        else -> "был(а) давно"
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,8 @@
 const val SECOND = 60
 
 fun main() {
-    val timeAgo = 60 * 11
+    val timeAgo = SECOND * 11// веди число (минуты)
 
-    println(
-        when {
-            timeAgo <= SECOND -> "был(а) только что"
-            timeAgo < SECOND * SECOND -> "был(а) ${timeAgo / SECOND} ${ending(timeAgo)} назад"
-            timeAgo < 24 * SECOND * SECOND -> "был(а) в сети ${timeAgo / (SECOND * SECOND)} ${ending(timeAgo)} назад"
-            timeAgo < 48 * SECOND * SECOND -> "был(а) сегодня"
-            timeAgo < 72 * SECOND * SECOND -> "был(а) вчера"
-            else -> "был(а) давно"
-        }
-    )
+    println(printending(timeAgo))
 }
-
-
 
