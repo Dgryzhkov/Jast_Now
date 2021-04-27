26cb4801ce347207e25d6a99e3af06dc039b472a
Dgryzhkov <dgryzhkov@gmail.com>
2021-04-27 10:33:22 +0700

    bug fixed

--- src/main/kotlin/ending.kt
+++ src/main/kotlin/ending.kt
@@ -5,24 +5,28 @@ const val ENDING_HOURS_OV = "ов"
 const val ENDING_MINUTES_YOU = "ы"
 const val ENDING_MINUTE_U = "у"
 
-fun minutesOrHours(time: Int): String {
-    return if (time in 0..3599) MINUTES else HOURS
+
+fun endingMinutes(time: Int): String {
+    return when (time / SECOND % 10) {
+        0, in 5..9 -> MINUTES
+        1 -> if ((time / SECOND) == 11) MINUTES else MINUTES + ENDING_MINUTE_U
+        else -> if ((time / (SECOND)) in 12..15) MINUTES else MINUTES + ENDING_MINUTES_YOU
+    }
 }
 
-fun ending(time: Int): String {
-    return when (if (minutesOrHours(time) == MINUTES) (time / SECOND % 10) else (time / (SECOND * SECOND) % 10)) {
-        0, in 5..9 -> if (minutesOrHours(time) == MINUTES) MINUTES else HOURS + ENDING_HOURS_OV
-        1 -> if (minutesOrHours(time) == MINUTES) if (time / SECOND == 11) MINUTES else MINUTES + ENDING_MINUTE_U else
-            if (((time / (SECOND * SECOND)) % 100) == 11) HOURS + ENDING_HOURS_OV else HOURS
-        else -> if (minutesOrHours(time) == MINUTES) MINUTES + ENDING_MINUTES_YOU else HOURS + ENDING_HOURS_A
+fun endingHours(time: Int): String {
+    return when (time / (SECOND * SECOND) % 10) {
+        0, in 5..9 -> HOURS + ENDING_HOURS_OV
+        1 -> if ((time / (SECOND * SECOND)) == 11) HOURS + ENDING_HOURS_OV else HOURS
+        else -> if ((time / (SECOND * SECOND)) in 12..15) HOURS + ENDING_HOURS_OV else HOURS + ENDING_HOURS_A
     }
 }
 
 fun printEnding(time: Int): String {
     return when {
         time <= SECOND -> "был(а) только что"
-        time < SECOND * SECOND -> "был(а) ${time / SECOND} ${ending(time)} назад"
-        time < 24 * SECOND * SECOND -> "был(а) в сети ${time / (SECOND * SECOND)} ${ending(time)} назад"
+        time < SECOND * SECOND -> "был(а) ${time / SECOND} ${endingMinutes(time)} назад"
+        time < 24 * SECOND * SECOND -> "был(а) в сети ${time / (SECOND * SECOND)} ${endingHours(time)} назад"
         time < 48 * SECOND * SECOND -> "был(а) сегодня"
         time < 72 * SECOND * SECOND -> "был(а) вчера"
         else -> "был(а) давно"
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,8 @@
 const val SECOND = 60
 
+
 fun main() {
-    val timeAgo = SECOND*SECOND*21 // веди число (минуты)
+    val timeAgo = SECOND// веди количество секунд
 
     println(printEnding(timeAgo))
 }
